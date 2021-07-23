50747710273d3dae12982aa6e71f383cb1fecb38
jrog20 <jaimerogstad@gmail.com>
2021-07-23 12:32:32 -0400

    Rename and Restructure components and container components

--- src/App.js
+++ src/App.js
@@ -10,20 +10,13 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <Header /> */}
-          {/* Render Hero Image Here */}
           Render Header component here: 
-          Nav Bar is rendering here
           <p>
-            {/* <h3>Hero image is rendering here:</h3>
-            <img src='https://scontent-bos3-1.xx.fbcdn.net/v/t1.6435-9/p720x720/121307571_3365457176863878_5338307208764464459_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=e3f864&_nc_ohc=1u2tWmuq_ZcAX-dZmAQ&_nc_ht=scontent-bos3-1.xx&oh=f25832aed5648714af2f34dd9f50034f&oe=60FE11C5' alt="Horse"></img> */}
-          </p>
-          
-          <p>
-            Main Content of Page
+            West River Stables
             <HomeContainer />
           </p>
         </header>
+        <HomeContainer />
       </div>
     );
   }
