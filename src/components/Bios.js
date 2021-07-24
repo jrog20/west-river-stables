d8a70beff8f79e11a96ea6d0305f0e2ef0271050
jrog20 <jaimerogstad@gmail.com>
2021-07-24 17:20:01 -0400

    Begin work for bios api

--- src/components/Bios.js
+++ src/components/Bios.js
@@ -27,6 +27,14 @@ class Bios extends React.Component {
     }
   }
 
+  // Need to connect to the Bios api like this?
+  // componentDidMount() {
+  //   api.get('/bios')
+  //     .then(bios => this.setState({
+  //       bios
+  //     }))
+  // }
+
   render() {
     return (
       this.state.bios.map(b => <BioCard key={b.name} bio={b} />)
