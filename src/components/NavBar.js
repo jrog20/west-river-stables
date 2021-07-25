13f6575f11b2634d23577be7023f6422e2c34fb2
jrog20 <jaimerogstad@gmail.com>
2021-07-25 14:23:01 -0400

    Add navbar css and begin creating hamburger nav menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './NavBar.css';
 
 const NavBar = () => {
   return (
-    <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
-      <NavLink style={{ marginRight: '10px' }} to="/">
-        Home
-      </NavLink>
-      <NavLink style={{ marginRight: '10px' }} to="/about">
-        About
-      </NavLink>
-      <NavLink style={{ marginRight: '10px' }} to="/services">
-        Services
-      </NavLink>
-      <NavLink style={{ marginRight: '10px' }} to="/contact">
-        Contact
-      </NavLink>
-    </div>
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <NavLink to='/' className='navbar-logo'>
+            WRS <i className='fab fa-typo3' />
+          </NavLink>
+          
+
+
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/services">Services</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </div>
+      </nav>
+    </>
   );
 }
 
